b5547c326eca896834679058ccc4210509769999
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-09-13 15:06:57 -0700

    started car event, viewing, and account creating website
    difficult project incoming

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const hhtp = require('http')
+var app = express()
+const PORT = 3000
+
+var peopleData = require('./data/main.json')
+
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        people: peopleData,
+    })
+})
+
+
+
+
+app.get('/about', (req, res) => {
+    res.render('pages/about', {
+        people: peopleData,
+    })
+})
+
+app.listen(PORT, PORT =>{
+    console.log(`listening on port ${PORT}...`);
+})
