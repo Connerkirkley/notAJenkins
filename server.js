0e224d32ba0dae67ddcfa1149b0c59cc600b74a3
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-09-18 15:16:03 -0700

    stuff

--- server.js
+++ server.js
@@ -5,13 +5,47 @@ const PORT = 5000
 
 
 
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
 
+let checkSessionLogin = (req) =>{
+    let peopleData = JSON.parse(fs.readFileSync('./data/main.json'))
+    peopleData.forEach(person => {
+        if(req.body.userName === person.userName && req.body.userPassword === person.userPassword){
+            if (person.admin === true){
+                return [person.id, true, true]
+            }
+            return [person.id, true, false]
+        }
+    })
+    return false
+}
+
 app.get('/', (req, res) => {
-    let peopleData = JSON.parse(fs.readFile('./data/main.json'))
+    let session = checkSessionLogin(req)
+
+    if (session[1] === true){
+        
+        res.render('pages/main', {
+            userid: session[0],
+            isLogin: session[1],
+            isAdmin: session[2],
+            events: JSON.parse(fs.readFileSync('./data/events.json'))
+        })
+    }
+    res.render('pages/login', {
+    })
+})
+
+
+
+
+
+app.get('/main', (req, res) => {
+    let peopleData = JSON.parse(fs.readFileSync('./data/main.json'))
     res.render('pages/main', {
         people: peopleData,
     })
